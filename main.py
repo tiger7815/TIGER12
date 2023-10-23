888b60c3f5f28c0205fe1bef3e1a22964cead72a
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-23 20:07:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -930,7 +930,7 @@ async def account_login(bot: Client, m: Message):
             else:
                 cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv "{url1}"'
             try:
-                download_cmd = f"{cmd} -R 25 --fragment-retries 25 --throttled-rate 4.2M --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
+                download_cmd = f"{cmd} -R 25 --fragment-retries 25 --throttled-rate 4M --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
                 os.system(download_cmd)
 
                 if os.path.isfile(f"{name}.mkv"):
