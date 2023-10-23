351d64a642e6ddcc0335780072893d762b2faca7
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-24 04:56:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -926,11 +926,11 @@ async def account_login(bot: Client, m: Message):
             prog = await m.reply_text(Show)
             cc = f'**Title »** {name1}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n '
             if "pdf" in url:
-                cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
+                cmd = f'youtube-dl -o "{name}.pdf" "{url1}"'
             else:
-                cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv "{url1}"'
+                cmd = f'youtube-dl -o "{name}.mp4" --no-keep-video --remux-video mkv "{url1}"'
             try:
-                download_cmd = f"{cmd} -R 25 --fragment-retries 25 --throttled-rate 4M --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
+                download_cmd = f"{cmd} -R 25 --fragment-retries 25 --throttled-rate 10k --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
                 os.system(download_cmd)
 
                 if os.path.isfile(f"{name}.mkv"):
