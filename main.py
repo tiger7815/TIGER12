0fab0798158e7fd4026a04528e155ac73ec618e4
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-24 09:13:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -928,7 +928,7 @@ async def account_login(bot: Client, m: Message):
             if "pdf" in url:
                 cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
-                cmd = f'youtube-dl -o {name}.mp4 {url1} --verbose'
+                cmd = f'youtube-dl -o "{name}.mp4" "{url1}" --verbose'
             try:
                 download_cmd = f"{cmd} --fragment-retries 25 --external-downloader aria2c --external-downloader-args '-x 16 -j 32' --verbose"
                 os.system(download_cmd)
