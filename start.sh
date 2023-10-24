b6c185b3b075dc284500674af0ab4da6e178445e
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-24 10:50:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 pip install --upgrade pip
 sudo apt install libcurl4-gnutls-dev librtmp-dev
 pip install pycurl
-pip install --upgrade youtube_dl
 pip install -r requirements.txt
+pip install --upgrade youtube_dl
 python main.py
