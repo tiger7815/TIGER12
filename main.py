c2ca0c26e3cab7fa99e5a8da36b6943644990280
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-11-27 01:09:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ logging = logging.getLogger()
 
 bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
-             api_id=int(os.environ.get("API_ID")),
+             api_id=int(os.environ.get("API_ID",22609670)),
              api_hash=os.environ.get("API_HASH"))
 auth_users = [6981453498]
 sudo_users = auth_users
